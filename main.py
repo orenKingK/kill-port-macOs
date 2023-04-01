861b6be2c6999e997615e90a0eada14565940645
orenKingK <oreni890@gmail.com>
2023-04-01 14:23:21 +0300

    add

--- main.py
+++ main.py
@@ -11,26 +11,24 @@ def clean_output_white_spaces(line_to_clean):
 
 
 def main():
-    port = input('Choose a port: ')
+    ports_str = input('Choose a ports with spaces: ')
+    ports = ports_str.split(' ')
+    for port in ports:
+        output_find_process_pid = send_command(f'sudo lsof -i :{port}')
+        cmd_output_lines = output_find_process_pid.stdout.readlines()
 
-    output_find_process_pid = send_command(f'sudo lsof -i :{port}')
-    cmd_output_lines = output_find_process_pid.stdout.readlines()
+        if len(cmd_output_lines) < 1:
+            print(f"No process at the chosen port {port}!")
+            continue
 
-    if len(cmd_output_lines) < 1:
-        print("No process at the chosen port !!")
-        return
+        pid_line = cmd_output_lines[1]
+        str_pid_line = pid_line.decode('utf-8')
+        str_pid_line = clean_output_white_spaces(str_pid_line)
+        pid = str_pid_line.split(' ')[1]
 
-    pid_line = cmd_output_lines[1]
-    str_pid_line = pid_line.decode('utf-8')
-    str_pid_line = clean_output_white_spaces(str_pid_line)
-    pid = str_pid_line.split(' ')[1]
+        send_command(f'kill -9 {pid}')
 
-    send_command(f'kill -9 {pid}')
-
-    print("Process at port: ", port, " killed :)")
-
-
-
+        print("Process at port: ", port, " killed :)")
 
 
 if __name__ == '__main__':
